3eaaf692c01f11422efcfc181a2433603db77ac9
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-04-29 16:06:09 +0300

    init thryve-backend spring application

---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/ThryveBackendApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.webjava.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThryveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThryveBackendApplication.class, args);
+    }
+
+}
