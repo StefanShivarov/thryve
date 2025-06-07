4bc13772622962a712f9d9ad4b9836c8906d68fd
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-07 15:36:34 +0300

    generate uuids with ulid

--- thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/model/entity/BaseEntity.java
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/model/entity/BaseEntity.java
@@ -1,9 +1,9 @@
 package bg.sofia.uni.fmi.webjava.backend.model.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import com.github.f4b6a3.ulid.UlidCreator;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Data;
 
 import java.util.UUID;
@@ -13,7 +13,13 @@ import java.util.UUID;
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @PrePersist
+    private void prePersist() {
+        if (id == null) {
+            id = UlidCreator.getUlid().toUuid();
+        }
+    }
 
 }
