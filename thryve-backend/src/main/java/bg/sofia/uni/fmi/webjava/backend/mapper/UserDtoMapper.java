59b9fe4b6bfb31e2e90d42a13b4214736607178e
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-08 01:30:45 +0300

    implement dto-entity mapping logic with mapstruct

---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/mapper/UserDtoMapper.java
@@ -0,0 +1,22 @@
+package bg.sofia.uni.fmi.webjava.backend.mapper;
+
+import bg.sofia.uni.fmi.webjava.backend.dto.CreateUserDto;
+import bg.sofia.uni.fmi.webjava.backend.dto.UpdateUserDto;
+import bg.sofia.uni.fmi.webjava.backend.dto.response.UserResponseDto;
+import bg.sofia.uni.fmi.webjava.backend.model.entity.User;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface UserDtoMapper {
+
+    User mapDtoToUser(CreateUserDto createUserDto);
+
+    UserResponseDto mapUserToResponseDto(User user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserFromDto(UpdateUserDto updateUserDto, @MappingTarget User user);
+
+}
