6ca5fad8c3a5d18f40317ba872ee2463866d13b2
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-07 15:37:01 +0300

    create empty initial jpa repositories

--- thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/repository/ResourceRepository.java
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/repository/ResourceRepository.java
@@ -1,12 +1,12 @@
 package bg.sofia.uni.fmi.webjava.backend.repository;
 
-import bg.sofia.uni.fmi.webjava.backend.model.entity.User;
+import bg.sofia.uni.fmi.webjava.backend.model.entity.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface ResourceRepository extends JpaRepository<Resource, UUID> {
 
 }
