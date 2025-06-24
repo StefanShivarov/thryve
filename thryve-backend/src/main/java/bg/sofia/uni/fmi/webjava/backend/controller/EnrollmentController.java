67a2c49528b62682a77f0d553d6896bf4fbf4948
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-25 01:44:46 +0300

    implement crud endpoints for enrollments api

---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/controller/EnrollmentController.java
@@ -0,0 +1,88 @@
+package bg.sofia.uni.fmi.webjava.backend.controller;
+
+import bg.sofia.uni.fmi.webjava.backend.model.dto.EntityModificationResponse;
+import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.CreateEnrollmentDto;
+import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.EnrollmentResponseDto;
+import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.UpdateEnrollmentDto;
+import bg.sofia.uni.fmi.webjava.backend.service.EnrollmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/enrollments")
+@RequiredArgsConstructor
+public class EnrollmentController {
+
+    private static final String ENROLLMENT_CREATED_MESSAGE = "Enrollment created successfully!";
+    private static final String ENROLLMENT_UPDATED_MESSAGE = "Enrollment updated successfully!";
+    private static final String ENROLLMENT_DELETED_MESSAGE = "Enrollment deleted successfully!";
+
+    private final EnrollmentService enrollmentService;
+
+    @GetMapping(value = {"", "/"})
+    public ResponseEntity<Page<EnrollmentResponseDto>> getEnrollmentsByCourseId(
+        @RequestParam(required = false) UUID userId,
+        @RequestParam(required = false) UUID courseId,
+        @RequestParam(defaultValue = "0") int pageNumber,
+        @RequestParam(defaultValue = "10") int pageSize,
+        @RequestParam(defaultValue = "id") String sortBy,
+        @RequestParam(defaultValue = "ASC") String direction
+    ) {
+        Sort.Direction sortDirection = Sort.Direction.fromOptionalString(direction).orElse(Sort.Direction.ASC);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortDirection, sortBy));
+
+        if (userId != null && courseId != null) {
+            return ResponseEntity.ok(enrollmentService.getEnrollmentsByCourseIdAndUserId(courseId, userId, pageable));
+        }
+        if (userId != null) {
+            return ResponseEntity.ok(enrollmentService.getEnrollmentsByUserId(userId, pageable));
+        }
+        if (courseId != null) {
+            return ResponseEntity.ok(enrollmentService.getEnrollmentsByCourseId(courseId, pageable));
+        }
+        return ResponseEntity.ok(enrollmentService.getAllEnrollments(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EnrollmentResponseDto> getEnrollmentById(@PathVariable UUID id) {
+        return ResponseEntity.ok(enrollmentService.getEnrollmentById(id));
+    }
+
+    @PostMapping(value = {"", "/"})
+    public ResponseEntity<EntityModificationResponse<EnrollmentResponseDto>> createEnrollment(@RequestBody @Valid CreateEnrollmentDto createEnrollmentDto) {
+        return ResponseEntity.ok(
+            new EntityModificationResponse<>(ENROLLMENT_CREATED_MESSAGE, enrollmentService.createEnrollment(createEnrollmentDto))
+        );
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<EntityModificationResponse<EnrollmentResponseDto>> updateEnrollmentById(@PathVariable UUID id, @RequestBody @Valid UpdateEnrollmentDto updateEnrollmentDto) {
+        return ResponseEntity.ok(
+            new EntityModificationResponse<>(ENROLLMENT_UPDATED_MESSAGE, enrollmentService.updateEnrollmentById(id, updateEnrollmentDto))
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<EntityModificationResponse<EnrollmentResponseDto>> deleteEnrollmentById(@PathVariable UUID id) {
+        return ResponseEntity.ok(
+            new EntityModificationResponse<>(ENROLLMENT_DELETED_MESSAGE, enrollmentService.deleteEnrollmentById(id))
+        );
+    }
+
+}
---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/service/EnrollmentService.java
@@ -0,0 +1,95 @@
+package bg.sofia.uni.fmi.webjava.backend.service;
+
+import bg.sofia.uni.fmi.webjava.backend.exception.EntityAlreadyExistsException;
+import bg.sofia.uni.fmi.webjava.backend.exception.EntityNotFoundException;
+import bg.sofia.uni.fmi.webjava.backend.mapper.EnrollmentDtoMapper;
+import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.CreateEnrollmentDto;
+import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.EnrollmentResponseDto;
+import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.UpdateEnrollmentDto;
+import bg.sofia.uni.fmi.webjava.backend.model.entity.Course;
+import bg.sofia.uni.fmi.webjava.backend.model.entity.Enrollment;
+import bg.sofia.uni.fmi.webjava.backend.model.entity.User;
+import bg.sofia.uni.fmi.webjava.backend.repository.EnrollmentRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+public class EnrollmentService {
+
+    public static final String ENROLLMENT_NOT_FOUND_ERROR_MESSAGE = "Enrollment with id %s was not found!";
+
+    private final EnrollmentRepository enrollmentRepository;
+    private final EnrollmentDtoMapper enrollmentDtoMapper;
+    private final CourseService courseService;
+    private final UserService userService;
+
+    @Transactional
+    public Page<EnrollmentResponseDto> getAllEnrollments(Pageable pageable) {
+        return enrollmentRepository.findAll(pageable)
+            .map(enrollmentDtoMapper::mapToResponseDto);
+    }
+
+    @Transactional
+    public Page<EnrollmentResponseDto> getEnrollmentsByCourseIdAndUserId(UUID courseId, UUID userId, Pageable pageable) {
+        return enrollmentRepository.findEnrollmentByCourseIdAndUserId(courseId, userId, pageable)
+            .map(enrollmentDtoMapper::mapToResponseDto);
+    }
+
+    @Transactional
+    public Page<EnrollmentResponseDto> getEnrollmentsByCourseId(UUID courseId, Pageable pageable) {
+        return enrollmentRepository.findEnrollmentsByCourseId(courseId, pageable)
+            .map(enrollmentDtoMapper::mapToResponseDto);
+    }
+
+    @Transactional
+    public Page<EnrollmentResponseDto> getEnrollmentsByUserId(UUID userId, Pageable pageable) {
+        return enrollmentRepository.findEnrollmentsByUserId(userId, pageable)
+            .map(enrollmentDtoMapper::mapToResponseDto);
+    }
+
+    @Transactional
+    public EnrollmentResponseDto getEnrollmentById(UUID id) {
+        Enrollment enrollment = enrollmentRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(format(ENROLLMENT_NOT_FOUND_ERROR_MESSAGE, id)));
+
+        return enrollmentDtoMapper.mapToResponseDto(enrollment);
+    }
+
+    @Transactional
+    public EnrollmentResponseDto createEnrollment(CreateEnrollmentDto dto) {
+        Enrollment enrollment = enrollmentDtoMapper.mapDtoToEnrollment(dto);
+        if (!getEnrollmentsByCourseIdAndUserId(dto.getCourseId(), dto.getUserId(), Pageable.unpaged()).isEmpty()) {
+            throw new EntityAlreadyExistsException("Enrollment with userId %s and courseId %s already exists!");
+        }
+        Course course = courseService.getCourseEntityById(dto.getCourseId());
+        User user = userService.getUserEntityById(dto.getUserId());
+        enrollment.setCourse(course);
+        enrollment.setUser(user);
+        return enrollmentDtoMapper.mapToResponseDto(enrollmentRepository.save(enrollment));
+    }
+
+    @Transactional
+    public EnrollmentResponseDto updateEnrollmentById(UUID id, UpdateEnrollmentDto updateEnrollmentDto) {
+        Enrollment enrollment = enrollmentRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(format(ENROLLMENT_NOT_FOUND_ERROR_MESSAGE, id)));
+
+        enrollmentDtoMapper.updateEnrollmentFromDto(updateEnrollmentDto, enrollment);
+        return enrollmentDtoMapper.mapToResponseDto(enrollmentRepository.save(enrollment));
+    }
+
+    @Transactional
+    public EnrollmentResponseDto deleteEnrollmentById(UUID id) {
+        EnrollmentResponseDto enrollmentResponseDto = getEnrollmentById(id);
+        enrollmentRepository.deleteById(id);
+        return enrollmentResponseDto;
+    }
+
+}
