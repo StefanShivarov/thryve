2fbd0a3d9949c4415e96e394819f318f21e6126f
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-29 17:54:30 +0300

    implement necessary repository methods

--- thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/repository/EnrollmentRequestRepository.java
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/repository/EnrollmentRequestRepository.java
@@ -1,12 +1,21 @@
 package bg.sofia.uni.fmi.webjava.backend.repository;
 
 import bg.sofia.uni.fmi.webjava.backend.model.entity.EnrollmentRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EnrollmentRequestRepository extends JpaRepository<EnrollmentRequest, UUID> {
+
+    Page<EnrollmentRequest> findEnrollmentRequestsByCourseId(UUID courseId, Pageable pageable);
+
+    Page<EnrollmentRequest> findEnrollmentRequestsByUserId(UUID userId, Pageable pageable);
+
+    Optional<EnrollmentRequest> findEnrollmentRequestByCourseIdAndUserId(UUID courseId, UUID userId);
 
 }
