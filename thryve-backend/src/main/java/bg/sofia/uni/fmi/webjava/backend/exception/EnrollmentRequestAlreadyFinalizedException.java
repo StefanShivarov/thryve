e8562cae462b93ec1dd57d77dae5f4a137f43ec1
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-29 17:55:55 +0300

    Add exception handling for already finalized requests

---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/exception/EnrollmentRequestAlreadyFinalizedException.java
@@ -0,0 +1,9 @@
+package bg.sofia.uni.fmi.webjava.backend.exception;
+
+public class EnrollmentRequestAlreadyFinalizedException extends RuntimeException {
+
+    public EnrollmentRequestAlreadyFinalizedException(String message) {
+        super(message);
+    }
+
+}
