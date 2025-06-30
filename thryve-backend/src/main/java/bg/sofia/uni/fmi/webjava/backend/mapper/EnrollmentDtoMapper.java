6eb1973063c90c5e20bb35f931da475f605e9119
StefanShivarov <stefan.shivarov.jr@gmail.com>
2025-06-30 19:58:35 +0300

    add bean mapping strategy for enrollment updates

--- thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/mapper/EnrollmentDtoMapper.java
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/mapper/EnrollmentDtoMapper.java
@@ -4,8 +4,10 @@ import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.EnrollmentCreateDto
 import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.EnrollmentResponseDto;
 import bg.sofia.uni.fmi.webjava.backend.model.dto.enrollment.EnrollmentUpdateDto;
 import bg.sofia.uni.fmi.webjava.backend.model.entity.Enrollment;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", uses = {UserDtoMapper.class, CourseDtoMapper.class})
 public interface EnrollmentDtoMapper {
@@ -14,6 +16,7 @@ public interface EnrollmentDtoMapper {
 
     EnrollmentResponseDto mapToResponseDto(Enrollment enrollment);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEnrollmentFromDto(EnrollmentUpdateDto enrollmentUpdateDto, @MappingTarget Enrollment enrollment);
 
 }
