98a7f2d62a59e702ebca3e29f286091ac0885c83
Serhan Chavdarliev <serhi1334@gmail.com>
2025-07-02 22:01:27 +0300

    added DTO's for resource

---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/model/dto/resource/ResourceCreateDto.java
@@ -0,0 +1,26 @@
+package bg.sofia.uni.fmi.webjava.backend.model.dto.resource;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResourceCreateDto {
+
+    @NotBlank(message = "Resource name cannot be blank!")
+    @Size(max = 200, message = "Resource name must be at most 200 characters long!")
+    private String name;
+
+    @NotBlank(message = "Resource URL cannot be blank!")
+    @Pattern(
+        regexp = "^https?://[^\\s/$.?#].\\S*$",
+        message = "Invalid resource URL!"
+    )
+    private String url;
+
+}
---
+++ thryve-backend/src/main/java/bg/sofia/uni/fmi/webjava/backend/model/dto/resource/ResourceUpdateDto.java
@@ -0,0 +1,21 @@
+package bg.sofia.uni.fmi.webjava.backend.model.dto.resource;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ResourceUpdateDto {
+
+    @Size(max = 200, message = "Resource name must be at most 200 characters long!")
+    private String name;
+
+    @Pattern(
+        regexp = "^https?://[^\\s/$.?#].\\S*$",
+        message = "Invalid resource URL!"
+    )
+    private String url;
+
+}
